616ec89ad5f671c6f8ee19085612bdd1136122d2
Max van Kampen <max.van.kampen@alicon.nl>
2021-05-21 10:17:15 +0200

    Auto commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+$().config({
+  client_id: "3666189-7ed01c7f-f2d7-4e61-9af4-8134ac66b16d",
+  info: {
+    title: "SourceForce",
+  },
+  client: {
+    servers: [
+      {
+        url: "https://sourceforce.aliconnect.nl/api"
+      }
+    ]
+  },
+});
